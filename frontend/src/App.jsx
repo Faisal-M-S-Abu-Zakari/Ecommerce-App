c1a992d54b21af4ce29bf9afaf841cdd68eb1d4c
zkryfysl <zkryfysl@email.com>
2026-02-04 13:38:50 +0200

    make the routes for the 9 pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Orders from "./pages/Orders";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+
 const App = () => {
+  // here i provided the padding x for differents sizes of the screen
   return (
-    <div>
-      <h1 class="font-bold text-3xl underline">Hello world!</h1>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* here i add the id to determine which product will open  */}
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
   );
 };
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Collections = () => {
+const Collection = () => {
   return <div></div>;
 };
 
-export default Collections;
+export default Collection;
