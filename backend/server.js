077f4adc1710d9b2a4cdb9bdfed895fe1e23a2dc
zkryfysl <zkryfysl@gmail.com>
2026-02-20 13:27:53 +0200

    Start the Server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config"; // this import will five me the support to use the .env file
+
+// App Config
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json()); // this middleware will allow us to parse the incoming request body as JSON
+app.use(cors()); // this middleware will allow us access to the backend from any IP .
+
+// api endpoints
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`listening on localhost:${port}`);
+});
